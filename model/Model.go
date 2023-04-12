fcefb25a3949f0f2c6e88d5a73084ab1f6544795
rookieQ <2399486893@qq.com>
2023-04-13 00:00:45 +0800

    修改model

--- model/Model.go
+++ model/Model.go
@@ -3,13 +3,12 @@ package model
 import "time"
 
 type Model struct {
-	Id       int64 `gorm:"primaryKey"`
-	CreateAt time.Time
-	UpdateAt time.Time
-	CreateBy int64
-	UpdateBy int64
-	Status   int16
-	Sort     int32
-	IsDelete bool
-	Remark   string
+	CreateTime time.Time
+	UpdateTime time.Time
+	CreateBy   int64
+	UpdateBy   int64
+	Status     int16
+	Sort       int32
+	IsDelete   bool
+	Remark     string
 }
